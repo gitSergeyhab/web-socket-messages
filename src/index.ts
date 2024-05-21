be2b3f9be96d3ff39f75b084646563d8b8ab6f14
Sergey <redmoloch@yandex.ru>
2024-05-22 01:09:08 +0300

    fix(for-dev): сменил порт

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 const hosts = process.env.ALLOWED_ORIGINS?.split(",") || [];
 
 // const origin = process.env.NODE_ENV === "development" ? hosts : "*";
-const origin = "*"; // TODO  заменить !!!
+const origin = "*";
 
 app.use(
   cors({
