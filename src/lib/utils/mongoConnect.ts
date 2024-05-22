59aa9f49a02923b3a98b4449659aad35d2b12f0d
Sergey <redmoloch@yandex.ru>
2024-05-22 21:34:38 +0300

    fix(fix-1): fix deploy.yml

--- src/lib/utils/mongoConnect.ts
+++ src/lib/utils/mongoConnect.ts
@@ -1,13 +1,8 @@
 import mongoose from "mongoose";
 import { logger } from "./logger";
 
-export const mongoConnect = () => {
-  logger.info(
-    `start mongo connection, hosts: ${process.env.ALLOWED_ORIGINS} / mon: ${process.env.MONGO_URI}`
-  );
+export const mongoConnect = () =>
   mongoose
     .connect(process.env.MONGO_URI!)
     .then(() => logger.info(`MongoDB connected`))
-    .catch((err) => logger.error(`MongoDB connection error:: ${err}`))
-    .finally(() => logger.info("end mongo connection"));
-};
+    .catch((err) => logger.error(`MongoDB connection error:: ${err}`));
