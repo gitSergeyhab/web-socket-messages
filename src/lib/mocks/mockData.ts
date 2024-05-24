2f9fdf1cc7a2d23b84815346ed0047fab95da1ce
gitSergeyhab <redmoloch@yandex.ru>
2024-05-24 10:19:37 +0300

    feat(NUTRI-45-2): Добавил моки для отправки на клиент хоть каких-то данных о несуществующих юзерах (#3)
    - добавил моки для юзеров
    - поправил getFirstAvatars
    - поправил имя файла

--- src/lib/mocks/mockData.ts
+++ src/lib/mocks/mockData.ts
@@ -1,4 +1,14 @@
-import { AuthUserData } from "../../types/user";
+import { AuthUserData, UserWithRoom } from "../../types/user";
+
+export const avatars = {
+  Hitchcock:
+    "https://pic.rutubelist.ru/video/e2/42/e24255c3eca77fc98f65ce7c3d857e7c.jpg",
+  Chaplin:
+    "https://s6.cdn.eg.ru/wp-content/uploads/2017/12/charli-chaplin092718.jpg",
+  Hepburn:
+    "https://mykaleidoscope.ru/uploads/posts/2022-06/1656469709_2-mykaleidoscope-ru-p-makiyazh-odri-khepbern-devushka-krasivo-fo-2.jpg",
+  Kelly: "https://yobte.ru/uploads/posts/2020-01/grejs-kelli-68-foto-38.jpg",
+};
 
 export const createMockedAuthUser = (
   user: Partial<AuthUserData> = {}
@@ -12,4 +22,42 @@ export const createMockedAuthUser = (
   ...user,
 });
 
-export const nockedAuthUserData = createMockedAuthUser();
+export const mockedAuthUserData = createMockedAuthUser();
+
+export const createMockedUserWithRoom = (
+  user: Partial<UserWithRoom> = {}
+): UserWithRoom => ({
+  ...mockedAuthUserData,
+  roomId: "1",
+  socketId: "any",
+  ...user,
+});
+
+export const mockedUserWithRoom = createMockedUserWithRoom();
+type UserWithRoomFroMap = [string, UserWithRoom];
+
+const mockedUsersInRoom3: UserWithRoomFroMap[] = Object.entries(avatars).map(
+  ([name, img], i) => [
+    name,
+    createMockedUserWithRoom({
+      avatar: img,
+      id: i,
+      socketId: name,
+      roomId: "3",
+    }),
+  ]
+);
+
+const mockedUsersInRoom1: UserWithRoomFroMap[] = [
+  ["Чарльз", mockedUserWithRoom],
+  [
+    "Hitchcock",
+    createMockedUserWithRoom({
+      avatar: avatars.Hitchcock,
+      id: 2,
+      socketId: avatars.Hitchcock,
+    }),
+  ],
+];
+
+export const usersInRooms = [...mockedUsersInRoom3, ...mockedUsersInRoom1];
